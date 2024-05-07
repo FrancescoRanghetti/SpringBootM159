4d7f2c77d0c63869ec6a79c5602ecb267e8c9eed
ranghetti <francesco.ranghetti@suntis.ch>
2024-05-07 08:40:57 +0200

    added logic for jwt token and authorization, created file yml

--- src/main/java/ch/sistemavotazioni/springbootm159/config/SecurityConfiguration.java
+++ src/main/java/ch/sistemavotazioni/springbootm159/config/SecurityConfiguration.java
@@ -1,11 +1,12 @@
 package ch.sistemavotazioni.springbootm159.config;
 
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.crypto.RsaKeyConversionServicePostProcessor;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,8 +14,14 @@ public class SecurityConfiguration extends WebSecurityConfiguration {
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    AuthenticationManagerBuilder authManBuild = http.getSharedObject(AuthenticationManagerBuilder.class);
-    http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((requests) -> requests.requestMatchers("/*").permitAll());
-    return http.build();
+    return http.authorizeHttpRequests(auth -> {
+      auth.requestMatchers("/**").permitAll();
+      auth.anyRequest().authenticated();
+    }).csrf(AbstractHttpConfigurer::disable).build();
+  }
+
+  @Bean
+  public static BeanFactoryPostProcessor conversionServicePostProcessor() {
+    return new RsaKeyConversionServicePostProcessor();
   }
 }
--- src/main/java/ch/sistemavotazioni/springbootm159/controller/LdapController.java
+++ src/main/java/ch/sistemavotazioni/springbootm159/controller/LdapController.java
@@ -1,50 +1,49 @@
 package ch.sistemavotazioni.springbootm159.controller;
 
 import ch.sistemavotazioni.springbootm159.entity.CredentialLogin;
+import ch.sistemavotazioni.springbootm159.service.JwtService;
 import ch.sistemavotazioni.springbootm159.service.LdapService;
-import java.util.Hashtable;
-import javax.naming.Context;
-import javax.naming.NamingException;
-import javax.naming.directory.DirContext;
-import javax.naming.directory.InitialDirContext;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/")
 public class LdapController {
 
-  private static final String LDAP_URL = "ldap://192.168.56.102:50000";
   private LdapService ldapService;
-  private UserDetailsContextMapper userDetailsContextMapper;
+  private JwtService jwtService;
   private final AuthenticationManager authenticationManager;
 
-  public LdapController(final AuthenticationManager authenticationManager, UserDetailsContextMapper userDetailsContextMapper, LdapService ldapService) {
+  public LdapController(final AuthenticationManager authenticationManager, LdapService ldapService, JwtService jwtService) {
     this.authenticationManager = authenticationManager;
-    this.userDetailsContextMapper = userDetailsContextMapper;
     this.ldapService = ldapService;
+    this.jwtService = jwtService;
   }
 
   @PostMapping("/loginLdap")
-  public ResponseEntity<UsernamePasswordAuthenticationToken> login(@RequestBody CredentialLogin credential) {
+  public ResponseEntity<Map<String, String>> login(@RequestBody CredentialLogin credential) {
     try {
-      System.out.println(credential.getName());
-      System.out.println(credential.getPassword());
       UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(credential.getName(), credential.getPassword());
       Authentication authentication = authenticationManager.authenticate(authenticationToken);
       SecurityContextHolder.getContext().setAuthentication(authentication);
-      System.out.println(authentication);
-      System.out.println(authenticationToken);
-      System.out.println(ldapService.getGroupFromUsername(credential.getName()));
-      return new ResponseEntity<>(authenticationToken, HttpStatus.OK);
+      String token = jwtService.generateToken(credential.getName(), getRole(credential.getName()));
+      Map response = new HashMap<>();
+      response.put("token", token);
+      return new ResponseEntity<>(response, HttpStatus.OK);
     } catch (Exception e) {
       System.out.println("Exception: " + e);
       e.printStackTrace();
@@ -52,30 +51,11 @@ public class LdapController {
     }
   }
 
-  @GetMapping("/ldap/health")
-  public ResponseEntity<String> checkLdap() {
-    boolean ldapHealth = checkLdapHealth();
-    if (ldapHealth) {
-      System.out.println("LDAP è raggiungibile e connesso.");
-      return ResponseEntity.ok().build();
-    } else {
-      System.out.println("LDAP non è raggiungibile o non è possibile connettersi.");
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Connessione non riuscita");
+  public String getRole(String name) {
+    String roleUser = ldapService.getGroupFromUsername(name).get(0).toString();
+    if (roleUser.contains("Administrator")) {
+      return "Administrator";
     }
-  }
-
-  public boolean checkLdapHealth() {
-    Hashtable<String, String> env = new Hashtable<>();
-    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-    env.put(Context.PROVIDER_URL, LDAP_URL);
-
-    try {
-      DirContext ctx = new InitialDirContext(env);
-      ctx.close();
-      return true;
-    } catch (NamingException e) {
-      e.printStackTrace();
-      return false;
-    }
+    return "User";
   }
 }
